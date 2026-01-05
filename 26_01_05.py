a4c93a2a65e68ae70c837d1010045465b2016e63
박지수 <trleresoo@gmail.com>
2026-01-05 17:10:46 +0900

    시도해봤는데 출력 안됨

--- 26_01_05.py
+++ 26_01_05.py
@@ -1,38 +1,11 @@
 # 클라우드가 만들어준 추가 연습
-# 1. 기본 흐름 익히기
+# 주석 보고 만들기 (challenge)
+# 1. 영화 3개 입력 받고 번호와 함께 출력
+# 1-1. 빈 리스트 movies 만들기
 movies = []
-movies.append("쉐이프 오브 워터")
-movies.append("라라랜드")
-movies.append("인터스텔라")
-
-for movie in movies:
-    print(movie)
-
-# 2. enumerate 익히기
-movies = ["쉐이프 오브 워터", "라라랜드", "인터스텔라"]
-
-for idx, movie in enumerate(movies):
-    print(f"{idx + 1}. {movie}")
-
-# 3. pop 익히기
-movies = ["쉐이프 오브 워터", "라라랜드", "인터스텔라"]
-# *pop은 ()안에 아무것도 안 적혀 있으면 마지막 요소를 삭제한다.
-deleted = movies.pop()
-print(f"삭제된 영화: {deleted}")
-print(f"남은 영화: {movies}")
-
-# 4. 입력 받아 추가하기
-movies = []
-# *movies로 빈 바구니를 먼저 만들고 range로 3번 반복
+# 1-2. 3번 반복하면서 영화 제목 입력 받아 추가하기
 for i in range(3):
-    movie = input("영화 제목: ")
-    movies.append(movie)
-print("등록된 영화:", movies)
-
-# 5. 검색하기
-movies = ["쉐이프 오브 워터", "라라랜드", "인터스텔라"]
-keyword = input("찾을 영화: ")
-
-for movie in movies:
-    if movie == keyword:
-        print(f"찾았습니다: {movie}")
+    movie = input()
+    # 1-3. enumerate 사용해서 번호와 함께 출력하기
+    for idx, movie in enumerate(movies):
+        print(idx,movies)
