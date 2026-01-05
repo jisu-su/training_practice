ad3e63af466ea3fddde091cff5200e262761c932
박지수 <trleresoo@gmail.com>
2026-01-05 17:50:38 +0900

    또또 다른 오류 방법

--- 26_01_05.py
+++ 26_01_05.py
@@ -8,5 +8,5 @@ for i in range(3):
     movie = input()
     movies.append(movie)
     # 1-3. enumerate 사용해서 번호와 함께 출력하기
-    for idx, movie in enumerate(movies):
-        print(idx,movies)
+for idx, movie in enumerate(movies):
+    print(idx+1,movies)
