a957d7c70acf3bf6385da586d42d9febc0acc8eb
박지수 <trleresoo@gmail.com>
2026-01-05 10:41:34 +0900

    섹션 A 연습 완료

---
+++ 26_01_05.py
@@ -0,0 +1,32 @@
+# 1. 변수 선언과 할당
+# 01. 문자열 변수 하나 선언
+name = "가나다"
+print(name)
+# 02. 숫자 변수 하나 선언
+age = 26
+print(age)
+# 03. 문자열 변수 두개 선언 후 연결
+first = "가"
+last = "나다"
+full = first + last
+print(full)
+# 04. 숫자 변수 두 개 선언 후 연산
+age = 26
+next_year = age + 1
+print(next_year)
+# 05. 변수에 새 값 재활당
+nickname = "가나다"
+nickname = "라마바"
+print(nickname)
+# 06. 변수 값을 다른 변수에 복사
+original = "가나다"
+copy = original
+print(copy)
+# 07. 입력 받은 값을 변수에 저장
+name = input()
+print(name)
+# 08. 입력 받은 두 값을 각각 변수에 저장
+name = input()
+age = input()
+print(name)
+print(age)
